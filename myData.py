a5e2718f0642fbb0a7d55f6d933d9765049e50a4
Momin <kmch43@hotmail.com>
2021-08-19 21:26:32 +1000

    tried adding new file

---
+++ myData.py
@@ -0,0 +1,17 @@
+
+def additionFunction(a,b=44):
+    #block of code
+    newLocalVar=13
+    print("a+b =", a+b)
+    return a+b
+
+# go outside of hte function body
+#call the fn
+
+
+var1=3
+var2=4
+
+storedResult= additionFunction(var1)
+
+    
